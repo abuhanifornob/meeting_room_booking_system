82d58c89ef0acc0f7f6425bcca3ef91c3dc3cd40
Abu Hanif <hanifcse90@gmail.com>
2024-08-28 17:33:23 +0600

    Check User Exists and Password check

--- src/app/modules/auth/auth.services.ts
+++ src/app/modules/auth/auth.services.ts
@@ -1,7 +1,23 @@
+import bcrypt from 'bcrypt';
+
+import { User } from '../user/user.model';
+
 import { TLogin } from './auth.interface';
 
 const authLogin = async (payload: TLogin) => {
-  console.log(payload);
+  // Check is user is Exists
+  const isUserExists = await User.findOne({ email: payload.email });
+  if (!isUserExists) {
+    throw new Error('User is Not Exiists');
+  }
+  // Check Password Match
+
+  const matchPassword = await bcrypt.compare(
+    payload.password,
+    isUserExists.password,
+  );
+  console.log(matchPassword);
+  return {};
 };
 
 export const LoginServices = {
